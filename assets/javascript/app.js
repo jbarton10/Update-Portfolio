6cd41a8e543c9c631649dd09d1b81be1b8cc6d6f
Jamie Barton <jbarton10@email.arizona.edu>
2019-07-03 11:57:45 -0700

    More HTML added need content infront of background image

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+
+$(".home").on("click", function(){
+
+});
+
+$(".about").on("click", function(){
+
+});
+
+$(".resume").on("click", function(){
+
+});
+
+$(".projects").on("click", function(){
+
+});
+
