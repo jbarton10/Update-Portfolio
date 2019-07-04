284fdb10fcefaded13c70e64c2b9c89ed70f032b
Jamie Barton <jbarton10@email.arizona.edu>
2019-07-03 18:22:21 -0700

    JS is working need to make content look better and add links ect

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,38 @@
+$("#homeContent").css("display", "inline");
+$("#resumeContent").css("display", "none");
+$("#bioContent").css("display", "none");
+$("#projectsContent").css("display", "none");
 
-$(".home").on("click", function(){
+$(".home").on("click", function () {
+    $("#homeContent").css("display", "inline");
+    $("#resumeContent").css("display", "none");
+    $("#bioContent").css("display", "none");
+    $("#projectsContent").css("display", "none");
 
-});
 
-$(".about").on("click", function(){
-
-});
-
-$(".resume").on("click", function(){
-
-});
-
-$(".projects").on("click", function(){
 
 });
 
+$(".bio").on("click", function () {
+    $("#homeContent").css("display", "none");
+    $("#resumeContent").css("display", "none");
+    $("#bioContent").css("display", "inline");
+    $("#projectsContent").css("display", "none");
+
+});
+
+$(".resume").on("click", function () {
+    $("#homeContent").css("display", "none");
+    $("#resumeContent").css("display", "inline");
+    $("#bioContent").css("display", "none");
+    $("#projectsContent").css("display", "none");
+
+})
+
+$(".projects").on("click", function () {
+    $("#homeContent").css("display", "none");
+    $("#resumeContent").css("display", "none");
+    $("#bioContent").css("display", "none");
+    $("#projectsContent").css("display", "inline");
+
+})
